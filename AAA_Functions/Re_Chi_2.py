b1013b57682edc5de741452390600ede5fa301f5
Acunajon <acunajon@gmail.com>
2017-04-04 11:53:59 -0700

    Delete Re_Chi_2.py

--- AAA_Functions/Re_Chi_2.py
+++
@@ -1,8 +0,0 @@
-"""GOAL: ROUTINE TO FOCUS THEOy TO DATx"""
-
-def Re_Chi_2(Dat, Theo, Uncert, DoF=2, Para=1):
-    
-    Chi = 0
-    for i in range(len(Dat)):
-        Chi += ((Dat[i] - Theo[i])**2)/(Uncert[i]**2)
-    return Chi/(DoF - Para)
